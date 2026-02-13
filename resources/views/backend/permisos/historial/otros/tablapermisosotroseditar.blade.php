6768b405e9f92a2ce636cd6df9715eaf8a48ce6e
Tatan <tatangamess@gmail.com>
2026-02-13 15:56:57 -0600

    revisiones para reportes2

---
+++ resources/views/backend/permisos/historial/otros/tablapermisosotroseditar.blade.php
@@ -0,0 +1,22 @@
+<table id="tabla" class="table table-bordered table-striped">
+    <thead>
+    <tr>
+        <th style="width: 25%">Nombre</th>
+        <th style="width: 25%">Unidad</th>
+        <th style="width: 25%">Cargo</th>
+        <th style="width: 8%">Opciones</th>
+    </tr>
+    </thead>
+    <tbody>
+    @foreach($arrayPermisos as $dato)
+        <tr>
+            <td>{{ $dato->nombre }}</td>
+            <td>
+                <button type="button"
+                        class="btn btn-info btn-xs" onclick="informacion({{ $dato->id }})">
+                    <i class="fas fa-edit" title="Editar"></i> Editar</button>
+            </td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
