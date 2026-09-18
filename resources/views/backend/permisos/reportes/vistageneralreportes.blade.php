6aef22a40808c1399c2870bd40a57f6f33646bdb
Tatan <tatangamess@gmail.com>
2026-09-18 11:21:55 -0600

    reporte nuevo

--- resources/views/backend/permisos/reportes/vistageneralreportes.blade.php
+++ resources/views/backend/permisos/reportes/vistageneralreportes.blade.php
@@ -216,9 +216,10 @@
             </div>
 
 
+
             <!-- Bloque: Información General -->
-            <div class="row justify-content-center">
-                <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-6">
                     <div class="card card-primary shadow">
                         <div class="card-header">
                             <h3 class="card-title">
